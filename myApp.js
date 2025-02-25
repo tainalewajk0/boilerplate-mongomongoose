46faebda9348ae42f9b0f5189ff76c82cc611755
Tai Nalewyko <tai.nalewajko@pm.me>
2025-02-25 20:05:55 +0000

    fixed some parentheses in line 90

--- myApp.js
+++ myApp.js
@@ -87,7 +87,7 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, {err, updatedDoc} => {
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, updatedDoc) => {
     if (err) return console.log(err);
     done(null, updatedDoc)
   });
